f23ba69be0ce87d9f6354f480c0d94b33b197c11
Laplaza1 <skysavage55@gmail.com>
2025-07-24 16:24:19 -0500

    17

--- M06/backend/app.js
+++ M06/backend/app.js
@@ -159,9 +159,8 @@ router.delete("/songs/:id",async(req,res)=>
             {
 
                 const song = await Song.findById(req.params.id)
-                console.log(req.params.id)
-                console.log(song._id)
-                const deleted = await Song.deleteOne({_id:req.params.id})
+                
+                const deleted = await Song.deleteOne({_id:song._id})
                 if(deleted)
                     {
                         res.sendStatus(204)
